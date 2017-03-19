de0d6b9a58364764b6bdc87a9110509e93a91ec9
dujinqiao <duzi1996@qq.com>
2017-03-19 10:38:11 +0800

    add components

--- public/src/js/components/ElegantShow/ElegantShow.js
+++ public/src/js/components/ElegantShow/ElegantShow.js
@@ -1,37 +1,41 @@
 import React, {Component} from "react";
-import IncubationBase from "./IncubationBase";
-import InnovationCommunities from "./InnovationCommunities";
-import InnovationLab from "./InnovationLab";
-import ItemShow from "./ItemShow";
-import PioneerCenter from "./PioneerCenter";
-import StarTeacher from "./StarTeacher";
-import StudentMien from "./StudentMien";
+import {IndexLink, Link} from "react-router";
+import "../../../css/elegantshow/elegantShow.css";
 
-export default class TelentShow extends Component {
-    render() {
-        return (
-            <div className="row">
-                <div className="col-sm-2" style={{border:"1px solid"}}>
-                    <ul>
-                        <li>明星导师</li>
-                        <li>优秀学生</li>
-                        <li>项目展示</li>
-                        <li>孵化基地</li>
-                        <li>大创中心</li>
-                        <li>创新实验室</li>
-                        <li>创新社团</li>
-                    </ul>
-                </div>
-                <div className="col-sm-8" style={{border:"1px solid"}}>
-                    <IncubationBase/>
-                    <InnovationCommunities/>
-                    <InnovationLab/>
-                    <ItemShow/>
-                    <PioneerCenter/>
-                    <StarTeacher/>
-                    <StudentMien/>
-                </div>
-            </div>
-        )
-    }
+export default class ElegantShow extends Component {
+  render() {
+    return (
+        <div className="elegant">
+          <div className="elegant-slide">
+            <ul>
+              <Link to="/elegantShow/starTeacher" activeStyle={{color: 'red'}}>
+                <li>明星导师</li>
+              </Link>
+              <Link to="/elegantShow/studentMien" activeStyle={{color: 'red'}}>
+                <li>优秀学生</li>
+              </Link>
+              <Link to="/elegantShow/itemShow" activeStyle={{color: 'red'}}>
+                <li>项目展示</li>
+              </Link>
+              <Link to="/elegantShow/incubationBase" activeStyle={{color: 'red'}}>
+                <li>孵化基地</li>
+              </Link>
+              <Link to="/elegantShow/pioneerCenter" activeStyle={{color: 'red'}}>
+                <li>大创中心</li>
+              </Link>
+              <Link to="/elegantShow/innovationLab" activeStyle={{color: 'red'}}>
+                <li>创新实验室</li>
+              </Link>
+              <IndexLink to="/elegantShow" activeStyle={{color: 'red'}}>
+                <li>创新社团</li>
+              </IndexLink>
+            </ul>
+          </div>
+          <div className="elegant-container">
+            {this.props.children}
+            {console.log(this.props.children)}
+          </div>
+        </div>
+    )
+  }
 }
--- public/src/js/components/ElegantShow/InnovationCommunities.js
+++ public/src/js/components/ElegantShow/InnovationCommunities.js
@@ -1,10 +1,50 @@
 import React, {Component} from "react";
-import "../../../css/telentshow/innovationCommutities.css";
 
-export default class InnovationCommunities extends Component{
-    render(){
-        return(
-            <div>i am innovation commutities!!</div>
-        )
-    }
+export default class InnovationCommunities extends Component {
+  render() {
+    return (
+        <div className="elegant-list">
+          <div className="elegant-list-item">
+            <div><img src="" alt="little image"/></div>
+            <div>
+              <h3>2017年中国在线少儿英语行业研究所</h3>
+              <p>本报告聚焦于我国少儿英语行业，从定性和定量两个角度，全面解读了当前行业的发展状况、发展经验和特点，并展望了未来的发展趋势。</p>
+              <time>发布时间：2017-10-2</time>
+            </div>
+          </div>
+          <div className="elegant-list-item">
+            <div><img src="" alt="little image"/></div>
+            <div>
+              <h3>2017年中国在线少儿英语行业研究所</h3>
+              <p>本报告聚焦于我国少儿英语行业，从定性和定量两个角度，全面解读了当前行业的发展状况、发展经验和特点，并展望了未来的发展趋势。</p>
+              <time>发布时间：2017-10-2</time>
+            </div>
+          </div>
+          <div className="elegant-list-item">
+            <div><img src="" alt="little image"/></div>
+            <div>
+              <h3>2017年中国在线少儿英语行业研究所</h3>
+              <p>本报告聚焦于我国少儿英语行业，从定性和定量两个角度，全面解读了当前行业的发展状况、发展经验和特点，并展望了未来的发展趋势。</p>
+              <time>发布时间：2017-10-2</time>
+            </div>
+          </div>
+          <div className="elegant-list-item">
+            <div><img src="" alt="little image"/></div>
+            <div>
+              <h3>2017年中国在线少儿英语行业研究所</h3>
+              <p>本报告聚焦于我国少儿英语行业，从定性和定量两个角度，全面解读了当前行业的发展状况、发展经验和特点，并展望了未来的发展趋势。</p>
+              <time>发布时间：2017-10-2</time>
+            </div>
+          </div>
+          <div className="elegant-list-item">
+            <div><img src="" alt="little image"/></div>
+            <div>
+              <h3>2017年中国在线少儿英语行业研究所</h3>
+              <p>本报告聚焦于我国少儿英语行业，从定性和定量两个角度，全面解读了当前行业的发展状况、发展经验和特点，并展望了未来的发展趋势。</p>
+              <time>发布时间：2017-10-2</time>
+            </div>
+          </div>
+        </div>
+    )
+  }
 }
