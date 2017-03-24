7306f301e838748f2fdae4a61d3211135bd867f8
gegeqi <ccczzz1015@163.com>
2017-03-24 13:44:54 +0800

    Synchronize git (#24)
    * add innovation data
    
    * add css to innovationdata.css

--- public/src/js/components/header.js
+++ public/src/js/components/header.js
@@ -12,6 +12,58 @@ class Frame_header extends Component {
                     </Link>
                 </div>
                 <div id="daohang">
+<<<<<<< HEAD
+                    <div className="header-menuHolder">
+                        <div className="header-menuWindow">
+                            <ul className="header-p1">
+                                <li className="header-s1"><a href="#url">
+                                    <div className="header-logo">网站导航</div>
+                                </a>
+                                    <ul className="header-p2">
+                                        <li className="header-s2"><a href="#"><span><div className="header-logo">首页</div></span></a>
+                                        </li>
+                                        <li className="header-s2"><Link to="/newscenter"><span><div className="header-logo">新闻中心</div></span></Link>
+                                            <ul className="header-p3 header-a3">
+                                                <li><a href="#">
+                                                    <div className="header-logo">西邮热点</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">媒体聚焦</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">公告通知</div>
+                                                </a></li>
+                                            </ul>
+                                        </li>
+                                        <li className="header-s2"><Link to="/elegantShow"><span><div className="header-logo">风采展示</div></span></Link>
+                                            <ul className="header-p3 header-a6">
+                                                <li><a href="#">
+                                                    <div className="header-logo">明星导师</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">学生风采</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">项目展示</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">孵化基地</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">大创中心</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">创新实验室</div>
+                                                </a></li>
+                                                <li><a href="#">
+                                                    <div className="header-logo">创新社团</div>
+                                                </a></li>
+
+                                            </ul>
+                                        </li>
+                                        <li className="header-s2"><Link to="innovationdata"><span><div className="header-logo">创新数据</div></span></Link>
+                                            <ul className="header-p3 header-a3">
+=======
                     <div className="menuHolder">
                         <div className="menuWindow">
                             <ul className="p1">
@@ -94,61 +146,73 @@ class Frame_header extends Component {
                                                 <span><div className="logo">创新数据</div></span>
                                             </Link>
                                             <ul className="p3 a3">
+>>>>>>> upstream/dev
                                                 <li><a href="#">
-                                                    <div className="logo">历年数据</div>
+                                                    <div className="header-logo">历年数据</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">历年孵化项目</div>
+                                                    <div className="header-logo">历年孵化项目</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创业图书馆</div>
+                                                    <div className="header-logo">创业图书馆</div>
                                                 </a></li>
                                             </ul>
                                         </li>
+<<<<<<< HEAD
+                                        <li className="header-s2"><Link to="onlineclass"><span><div className="header-logo">在线课堂</div></span></Link>
+                                            <ul className="header-p3 header-a3">
+=======
                                         <li className="s2">
                                             <Link to="onlineclass">
                                                 <span><div className="logo">在线课堂</div></span>
                                             </Link>
                                             <ul className="p3 a3">
+>>>>>>> upstream/dev
                                                 <li><a href="#">
-                                                    <div className="logo">尔雅基础教程</div>
+                                                    <div className="header-logo">尔雅基础教程</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">西邮开放实验</div>
+                                                    <div className="header-logo">西邮开放实验</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创业模拟沙盘</div>
+                                                    <div className="header-logo">创业模拟沙盘</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">大学生职业规划评测</div>
+                                                    <div className="header-logo">大学生职业规划评测</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创业基础慕课</div>
+                                                    <div className="header-logo">创业基础慕课</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创新创业实训课程</div>
+                                                    <div className="header-logo">创新创业实训课程</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">spoc微课</div>
+                                                    <div className="header-logo">spoc微课</div>
                                                 </a></li>
                                             </ul>
                                         </li>
+<<<<<<< HEAD
+                                        <li className="header-s2 header-b6"><Link to="/community"><span><div
+                                            className="header-logo">互动社区</div></span></Link>
+                                            <ul className="header-p3 header-a5">
+=======
                                         <li className="s2 b6">
                                             <Link to="/community">
                                                 <span><div className="logo">互动社区</div></span>
                                             </Link>
                                             <ul className="p3 a5">
+>>>>>>> upstream/dev
                                                 <li><a href="#">
-                                                    <div className="logo">创业竞赛交流专区</div>
+                                                    <div className="header-logo">创业竞赛交流专区</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创业团队交流专区</div>
+                                                    <div className="header-logo">创业团队交流专区</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创业项目交流专区</div>
+                                                    <div className="header-logo">创业项目交流专区</div>
                                                 </a></li>
                                                 <li><a href="#">
-                                                    <div className="logo">创新技能交流专区</div>
+                                                    <div className="header-logo">创新技能交流专区</div>
                                                 </a></li>
                                             </ul>
                                         </li>
