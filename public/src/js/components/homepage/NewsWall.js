804056e12450189f292bcb44b8aa265ff7384345
= <=>
2017-04-05 18:47:01 +0800

    change css on newWall

--- public/src/js/components/homepage/NewsWall.js
+++ public/src/js/components/homepage/NewsWall.js
@@ -18,7 +18,7 @@ class NewsWall extends Component {
         con.innerHTML += con.innerHTML;
 
         var timer = setInterval(function () {
-            if (scrollWrap.scrollTop == con.offsetHeight) {
+            if (scrollWrap.scrollTop >= con.scrollHeight/2) {
                 scrollWrap.scrollTop = 0;
             }
             scrollWrap.scrollTop += 2;
@@ -36,7 +36,7 @@ class NewsWall extends Component {
         var con = this.state.con;
         var timer = setInterval(function () {
 
-            if (scrollWrap.scrollTop == con.offsetHeight) {
+            if (scrollWrap.scrollTop >= con.scrollHeight/2) {
                 scrollWrap.scrollTop = 0;
             }
             scrollWrap.scrollTop += 2;
@@ -53,22 +53,22 @@ class NewsWall extends Component {
                     <div className="XU-hot-news-list" ref="scrollWrap" onMouseOver={this.onMouseOver.bind(this)}
                          onMouseOut={this.onMouseOut.bind(this)}>
                         <div ref="con1">
-                            <div className="hot-news-list hot-news-row1">新闻001,新闻001,新闻001<span
+                            <div className="hot-news-list hot-news-row1">我校积极参与陕西省地方立法工作...<span
                                 className="hot-news-row-span">3-28</span>
                             </div>
-                            <div className="hot-news-list hot-news-row2">新闻001,新闻001,新闻002<span
+                            <div className="hot-news-list hot-news-row2">我校积极参与陕西省地方立法工作...<span
                                 className="hot-news-row-span">3-28</span>
                             </div>
-                            <div className="hot-news-list hot-news-row3">新闻001,新闻001,新闻003<span
+                            <div className="hot-news-list hot-news-row3">我校积极参与陕西省地方立法工作...<span
                                 className="hot-news-row-span">3-28</span>
                             </div>
-                            <div className="hot-news-list hot-news-row4">新闻001,新闻001,新闻004<span
+                            <div className="hot-news-list hot-news-row4">我校积极参与陕西省地方立法工作...<span
                                 className="hot-news-row-span">3-28</span>
                             </div>
-                            <div className="hot-news-list hot-news-row5">新闻001,新闻001,新闻005<span
+                            <div className="hot-news-list hot-news-row5">我校积极参与陕西省地方立法工作...<span
                                 className="hot-news-row-span">3-28</span>
                             </div>
-                            <div className="hot-news-list hot-news-row6">新闻001,新闻001,新闻006<span
+                            <div className="hot-news-list hot-news-row6">我校积极参与陕西省地方立法工作...<span
                                 className="hot-news-row-span">3-28</span>
                             </div>
                         </div>
@@ -80,17 +80,17 @@ class NewsWall extends Component {
                     <div className="view-box-p">通知公告</div>
                     <img className="hot-notice-img" src=""/>
                     <div className="XU-hot-notice-list">
-                        <div className="hot-news-list hot-notice-row1">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row1">我校积极参与陕西省地方立法工作...<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row2">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row2">我校积极参与陕西省地方立法工作...<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row3">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row3">我校积极参与陕西省地方立法工作...<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row4">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row4">我校积极参与陕西省地方立法工作...<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row5">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row5">我校积极参与陕西省地方立法工作...<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row6 hot-last-view">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row6 hot-last-view">我校积极参与陕西省地方立法工作...<span
                             className="hot-news-row-span">3-28</span></div>
                     </div>
 
