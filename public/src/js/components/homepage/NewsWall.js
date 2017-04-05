b8efc178693a7d95dcb7fbd7586b668a3e327bb0
BubbleM <1870408473@qq.com>
2017-04-05 19:49:02 +0800

    refresh layout and font-size

--- public/src/js/components/homepage/NewsWall.js
+++ public/src/js/components/homepage/NewsWall.js
@@ -18,10 +18,10 @@ class NewsWall extends Component {
         con.innerHTML += con.innerHTML;
 
         var timer = setInterval(function () {
-            if (scrollWrap.scrollTop == con.offsetHeight) {
+            if (scrollWrap.scrollTop >= con.scrollHeight) {
                 scrollWrap.scrollTop = 0;
             }
-            scrollWrap.scrollTop += 1;
+            scrollWrap.scrollTop += 2;
         }, 20);
 
         this.setState({timer, scrollWrap, con});
@@ -36,7 +36,7 @@ class NewsWall extends Component {
         var con = this.state.con;
         var timer = setInterval(function () {
 
-            if (scrollWrap.scrollTop == con.offsetHeight) {
+            if (scrollWrap.scrollTop >= con.scrollHeight) {
                 scrollWrap.scrollTop = 0;
             }
             scrollWrap.scrollTop += 2;
@@ -49,7 +49,7 @@ class NewsWall extends Component {
             <div className="newswall-view-box">
                 <div className="XU-hot-news">
                     <div className="view-box-p">热点新闻</div>
-                    <img className="hot-news-img" src=""/>
+                    <img className="hot-news-img" src="../../../../image/newscenter/02.jpg"/>
                     <div className="XU-hot-news-list" ref="scrollWrap" onMouseOver={this.onMouseOver.bind(this)}
                          onMouseOut={this.onMouseOut.bind(this)}>
                         <div ref="con1">
@@ -78,19 +78,19 @@ class NewsWall extends Component {
 
                 <div className="XU-hot-notice">
                     <div className="view-box-p">通知公告</div>
-                    <img className="hot-notice-img" src=""/>
+                    <img className="hot-notice-img" src="../../../../image/newscenter/01.jpg"/>
                     <div className="XU-hot-notice-list">
-                        <div className="hot-news-list hot-notice-row1">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row1">我是通知公告,我是通知公告,yee!!<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row2">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row2">我是通知公告,我是通知公告,yee!!<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row3">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row3">我是通知公告,我是通知公告,yee!!<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row4">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row4">我是通知公告,我是通知公告,yee!!<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row5">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row5">我是通知公告,我是通知公告,yee!!<span
                             className="hot-news-row-span">3-28</span></div>
-                        <div className="hot-news-list hot-notice-row6 hot-last-view">我是通知公告,yee!!我是通知公告,yee!!<span
+                        <div className="hot-news-list hot-notice-row6 hot-last-view">我是通知公告,我是通知公告,yee!!<span
                             className="hot-news-row-span">3-28</span></div>
                     </div>
 
@@ -98,10 +98,16 @@ class NewsWall extends Component {
 
                 <div className="XU-hot-view">
                     <div className="view-box-p">风采视频</div>
-                    <img className="hot-view-img" src=""/>
+                    <img className="hot-view-img" src="" alt=""/>
                     <div className="XU-hot-view-list">
-                        <div className="hot-news-list hot-view-row1"></div>
-                        <div className="hot-news-list hot-view-row2"></div>
+                        <div className="hot-news-list hot-view-row1">
+                            <video className="hot-video" src="http://ofmz75843.bkt.clouddn.com/%E8%BD%AF%E5%B7%A5%E6%9D%AF%E9%A1%B9%E7%9B%AE%E4%BB%8B%E7%BB%8D%E8%A7%86%E9%A2%91.mp4" controls="controls">
+                            </video>
+                        </div>
+                        <div className="hot-news-list hot-view-row2">
+                            <video className="hot-video" src="http://ofmz75843.bkt.clouddn.com/%E8%BD%AF%E5%B7%A5%E6%9D%AF%E9%A1%B9%E7%9B%AE%E4%BB%8B%E7%BB%8D%E8%A7%86%E9%A2%91.mp4" controls="controls">
+                            </video>
+                        </div>
                     </div>
                 </div>
             </div>
