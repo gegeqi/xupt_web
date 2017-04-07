58e59473291118c22791b23a44559f89b774a216
egrils <angela335889390@gmail.com>
2017-04-07 21:09:30 +0800

    add imageWall to top_newscenter and change rotation_speed

--- public/src/js/components/newscenter/NewsCenter.js
+++ public/src/js/components/newscenter/NewsCenter.js
@@ -1,11 +1,31 @@
 import React, {Component} from "react";
 import {Link} from "react-router";
 import "../../../css/newscenter/NewsCenter.css";
+import Rotation from "../wheel/Rotation"
 
 export default class NewsCenter extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            imagesSrc: [
+                {src: "../../../image/innovationData/1.jpg"},
+                {src: "../../../image/innovationData/2.jpg"},
+                {src: "../../../image/innovationData/3.jpg"},
+                {src: "../../../image/innovationData/4.jpg"},
+                {src: "../../../image/innovationData/5.jpg"},
+                {src: "../../../image/innovationData/6.jpg"},
+                {src: "../../../image/innovationData/7.jpg"},
+                {src: "../../../image/innovationData/8.jpg"},
+                {src: "../../../image/innovationData/9.jpg"}
+            ]
+        };
+    }
     render() {
         return (
             <div>
+                <div className="topRotation">
+                    <Rotation imagesSrc={this.state.imagesSrc}/>
+                </div>
                 <div className="newsCenter row">
                     <div className="newsNavigation col-md-3">
                         <Link to='/newscenter'><li>西邮热点</li></Link>
