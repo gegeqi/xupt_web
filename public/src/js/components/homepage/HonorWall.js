0506ab047f722608a9ce7cd9bb904610a29e5134
egrils <angela335889390@gmail.com>
2017-04-07 16:26:10 +0800

    fix_honor_id

--- public/src/js/components/homepage/HonorWall.js
+++ public/src/js/components/homepage/HonorWall.js
@@ -6,7 +6,7 @@ export default class HonorWall extends Component {
         super(props);
     }
     componentDidMount(){
-        var tab1=document.getElementById("showImages");
+        var tab1=document.getElementById("showImages1");
         var tab2=document.getElementById("copyImages1");
         tab2.innerHTML=tab1.innerHTML;
         this.makeBigImage();
@@ -20,18 +20,18 @@ export default class HonorWall extends Component {
                 this.myTitle = this.title;
                 this.title = "";
                 var imgTitle = this.myTitle ? "<br/>" + this.myTitle : "";
-                var tooltip = "<div id='tooltip'><img src='"+ $(this).attr("src") +"' width='500px' height='300px' alt='产品预览图'/>"+imgTitle+"<\/div>"; //创建 div 元素
+                var tooltip = "<div id='tooltip1'><img src='"+ $(this).attr("src") +"' width='500px' height='300px' alt='产品预览图'/>"+imgTitle+"<\/div>"; //创建 div 元素
                 $("body").append(tooltip);	//把它追加到文档中
-                $("#tooltip")
+                $("#tooltip1")
                     .css({
                         "top": (e.pageY-300-y) + "px",
                         "left":  (e.pageX+x)  + "px"
                     }).show("fast");	  //设置x坐标和y坐标，并且显示
             }).mouseout(function(){
                 this.title = this.myTitle;
-                $("#tooltip").remove();	 //移除
+                $("#tooltip1").remove();	 //移除
             }).mousemove(function(e){
-                $("#tooltip")
+                $("#tooltip1")
                     .css({
                         "top": (e.pageY-300-y) + "px",
                         "left":  (e.pageX+x)  + "px"
@@ -49,9 +49,9 @@ export default class HonorWall extends Component {
 
         return (
 
-                <div id="honorWall">
-                    <div id="honorwrap">
-                        <div id="showImages">
+                <div id="honorWall1">
+                    <div id="honorwrap1">
+                        <div id="showImages1">
                             {honorImages}
                         </div>
                         <div id="copyImages1" className="copyImage"></div>
