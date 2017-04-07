50e1eca946d3a74a897ae831e33768bc9fb6b1d0
BubbleM <1870408473@qq.com>
2017-04-07 14:34:25 +0800

    add onlineclass erya file

--- public/src/js/components/onlineclass/BaseMooc.js
+++ public/src/js/components/onlineclass/BaseMooc.js
@@ -30,14 +30,14 @@ export default class BaseMooc extends Component {
                     <ul id="left_ul myCarousel">
                         <Link to='/onlineclass/erya'>
                             <li onMouseOver={this.mouseoverHandler0}>
-                                <a href="#">网课一链接</a>
+                                <a href="#">超星尔雅</a>
                             </li>
                         </Link>
-                        <li onMouseOver={this.mouseoverHandler1}><a href="#">网课二链接</a></li>
-                        <li onMouseOver={this.mouseoverHandler2}><a href="#">网课三链接</a></li>
-                        <li onMouseOver={this.mouseoverHandler3}><a href="#">网课四链接</a></li>
-                        <li onMouseOver={this.mouseoverHandler4}><a href="#">网课n链接</a></li>
-                        <li onMouseOver={this.mouseoverHandler5}><a href="#">SPOC链接</a></li>
+                        <li onMouseOver={this.mouseoverHandler1}><a href="#">西邮开放实验</a></li>
+                        <li onMouseOver={this.mouseoverHandler2}><a href="#">创业模拟沙盘</a></li>
+                        <li onMouseOver={this.mouseoverHandler3}><a href="#">大学生职业规划</a></li>
+                        <li onMouseOver={this.mouseoverHandler4}><a href="#">创业基础幕课</a></li>
+                        <li onMouseOver={this.mouseoverHandler5}><a href="#">创新创业实训</a></li>
                     </ul>
                 </div>
                 <div id="right">
