2f0e95672dc635dff4f83586e2fa6312178ab9bb
gegeqi <ccczzz1015@163.com>
2017-04-08 10:56:52 +0800

    change innovatition (#51)
    * add innovation data
    
    * add css to innovationdata.css
    
    * change index-page
    
    * add newsWall.css and newsWall.js
    
    * change newsWall
    
    * change newsWall (#35)
    
    * merge on dev (#29)
    
    * add innovation data
    
    * add css to innovationdata.css
    
    * change index-page
    
    * Achieve second leave nav
    
    * add honorWall
    
    * change newscenter_margin-top
    
    * change_lunbo_margin-top
    
    * Update style of ElegantShow
    
    * add source (#34)
    
    * add innovation data
    
    * add css to innovationdata.css
    
    * change index-page
    
    * add newsWall.css and newsWall.js
    
    * change css on newWall
    
    * change newswall.css and js
    
    * change header.js
    
    * change newswall.css newswall.js header.js
    
    * merge newsWall.js
    
    * change crash
    
    * change innovationdata
    
    * add innovariondata.css
    
    * add footer
    
    * change innovationdata
    
    * change package.json
    
    * the last master
    
    * del tab1 tab2
    
    * del login
    
    * change webpack.config

--- public/src/js/components/innovationData/AwardItems.js
+++ public/src/js/components/innovationData/AwardItems.js
@@ -1,9 +1,75 @@
 import React, {Component} from "react";
+import Pagination from "../Pagination";
 
 export default class AwardItems extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            allItems: [
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_18.png"
+                },
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_17.png"
+                },
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_16.png"
+                },
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_15.png"
+                },
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_14.png"
+                },
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_13.png"
+                },
+                {
+                    title: "周里波事件",
+                    content: "【周立波持枪持毒案开审，律师到庭后一言不发】周立波持枪持毒案在美国正式开庭审理，如果罪名成立，或将面临2年以上徒刑。最新现场消息，纽约时间4月7日中午12点50分左右，周立波代理律师莫虎现身，面对媒体发问，只字不语",
+                    time: "2017-4-8",
+                    image: "sj_icon_12.png"
+                }
+            ]
+        }
+    }
+
     render() {
+        const item = function (item) {
+            return (
+                <div className="competition-list-item">
+                    <div><img src={"../../../../image/article-icon/" + item.image} alt="little image"/></div>
+                    <div>
+                        <h3 className="competition-h3">{item.title}</h3>
+                        <p className="competition-p">{item.content}</p>
+                        发布时间：
+                        <time className="competition-time">{item.time}</time>
+                    </div>
+                </div>
+            )
+        };
+
         return (
-            <div>i am Awarditems</div>
+            <Pagination list={this.state.allItems} item={item} listStyle="competition-list" pageSize="5"/>
         )
     }
 }
