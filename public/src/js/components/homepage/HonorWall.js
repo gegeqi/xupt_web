7dbd796a732b5ab64c70ce12683f13e40621b47e
gegeqi <ccczzz1015@163.com>
2017-04-08 08:46:40 +0800

    merge dev o 4.8 (#49)
    * add innovation data
    
    * add css to innovationdata.css
    
    * change index-page
    
    * add newsWall.css and newsWall.js
    
    * change newsWall
    
    * change newsWall (#35)
    
    * merge on dev (#29)
    
    * add innovation data
    
    * add css to innovationdata.css
    
    * change index-page
    
    * Achieve second leave nav
    
    * add honorWall
    
    * change newscenter_margin-top
    
    * change_lunbo_margin-top
    
    * Update style of ElegantShow
    
    * add source (#34)
    
    * add innovation data
    
    * add css to innovationdata.css
    
    * change index-page
    
    * add newsWall.css and newsWall.js
    
    * change css on newWall
    
    * change newswall.css and js
    
    * change header.js
    
    * change newswall.css newswall.js header.js
    
    * merge newsWall.js
    
    * change crash
    
    * change innovationdata
    
    * add innovariondata.css
    
    * add footer
    
    * change innovationdata
    
    * change package.json
    
    * the last master

--- public/src/js/components/homepage/HonorWall.js
+++ public/src/js/components/homepage/HonorWall.js
@@ -2,46 +2,47 @@ import React, {Component} from "react";
 import "../../../css/homepage/honorWall.css";
 
 export default class HonorWall extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
-    componentDidMount(){
-        var tab1=document.getElementById("showImages1");
-        var tab2=document.getElementById("copyImages1");
-        tab2.innerHTML=tab1.innerHTML;
+
+    componentDidMount() {
+        var tab1 = document.getElementById("showImages1");
+        var tab2 = document.getElementById("copyImages1");
+        tab2.innerHTML = tab1.innerHTML;
         this.makeBigImage();
     }
 
-    makeBigImage(){
-        $(function(){
+    makeBigImage() {
+        $(function () {
             var x = 10;
             var y = 20;
-            $(".honorImage img").mouseover(function(e){
+            $(".honorImage img").mouseover(function (e) {
                 this.myTitle = this.title;
                 this.title = "";
                 var imgTitle = this.myTitle ? "<br/>" + this.myTitle : "";
-                var tooltip = "<div id='tooltip1'><img src='"+ $(this).attr("src") +"' width='500px' height='300px' alt='产品预览图'/>"+imgTitle+"<\/div>"; //创建 div 元素
+                var tooltip = "<div id='tooltip1'><img src='" + $(this).attr("src") + "' width='500px' height='300px' alt='产品预览图'/>" + imgTitle + "<\/div>"; //创建 div 元素
                 $("body").append(tooltip);	//把它追加到文档中
                 $("#tooltip1")
                     .css({
-                        "top": (e.pageY-300-y) + "px",
-                        "left":  (e.pageX+x)  + "px"
+                        "top": (e.pageY - 300 - y) + "px",
+                        "left": (e.pageX + x) + "px"
                     }).show("fast");	  //设置x坐标和y坐标，并且显示
-            }).mouseout(function(){
+            }).mouseout(function () {
                 this.title = this.myTitle;
                 $("#tooltip1").remove();	 //移除
-            }).mousemove(function(e){
+            }).mousemove(function (e) {
                 $("#tooltip1")
                     .css({
-                        "top": (e.pageY-300-y) + "px",
-                        "left":  (e.pageX+x)  + "px"
+                        "top": (e.pageY - 300 - y) + "px",
+                        "left": (e.pageX + x) + "px"
                     });
             });
         });
     }
 
     render() {
-        const honorImages = this.props.imagesSrc.map((ele,index)=>{
+        const honorImages = this.props.imagesSrc.map((ele, index)=> {
             return <div key={index} className="honorli honorImage">
                 <a href="#"><img src={ele.src}/></a>
             </div>
@@ -49,14 +50,14 @@ export default class HonorWall extends Component {
 
         return (
 
-                <div id="honorWall1">
-                    <div id="honorwrap1">
-                        <div id="showImages1">
-                            {honorImages}
-                        </div>
-                        <div id="copyImages1" className="copyImage"></div>
+            <div id="honorWall1">
+                <div id="honorwrap1">
+                    <div id="showImages1">
+                        {honorImages}
                     </div>
+                    <div id="copyImages1" className="copyImage"></div>
                 </div>
+            </div>
         )
     }
 }
